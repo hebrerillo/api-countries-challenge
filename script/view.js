3f12903723532631bb5feb7fab8b4a4504ccd02d
hebrerillo <hebrerillo@gmail.com>
2022-09-13 13:00:56 +0200

    Showing and hiding the regions.

--- script/view.js
+++ script/view.js
@@ -1,7 +1,37 @@
-class View {
-    
-    switchMode() {
+class View
+{
+
+    #selectRegion;
+
+    constructor()
+    {
+        this.#selectRegion = document.querySelector('.select-region');
+        this.setEvents();
+    }
+
+    /**
+     * Sets the initial events.
+     */
+    setEvents()
+    {
+        this.#selectRegion.addEventListener('click', this.toggleRegionsDisplay.bind(this));
+    }
+
+    /**
+     * Switches between dark and light mode.
+     *
+     */
+    switchMode()
+    {
         document.documentElement.classList.toggle('dark-theme');
+    }
+
+    /**
+     * Shows and hides the regions options.
+     */
+    toggleRegionsDisplay()
+    {
+        this.#selectRegion.closest('.select-region-wrapper').classList.toggle('display--regions');
     }
 }
 
